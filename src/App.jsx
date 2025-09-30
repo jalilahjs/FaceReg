5bc5a0b1a863ca799e2ca29bf83eba7ba21fa256
Jalilah Jaafar Sidek <jalilah@MacBook-Air.local>
2025-09-30 12:26:54 +0200

    move image recognition to the BE

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,6 @@ import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
 import "./App.css";
 
-const returnClarifaiRequestOptions = (imageURL) => {
-  const PAT = import.meta.env.VITE_API_PAT;
-  const USER_ID = import.meta.env.VITE_API_USER_ID;
-  const APP_ID = import.meta.env.VITE_API_APP_ID;
-
-  const raw = JSON.stringify({
-    user_app_id: { user_id: USER_ID, app_id: APP_ID },
-    inputs: [{ data: { image: { url: imageURL } } }],
-  });
-
-  return {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Key " + PAT,
-    },
-    body: raw,
-  };
-};
-
 const initialState = {
   input: "",
   imageURL: "",
@@ -48,18 +28,19 @@ class App extends Component {
   loadUser = (data) => this.setState({ user: data });
 
   calculateFaceLocations = (data) => {
-    if (!data.outputs || !data.outputs[0].data.regions) return [];
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
 
-    return data.outputs[0].data.regions.map((region) => {
-      const box = region.region_info.bounding_box;
+    // Clarifai returns regions array → each region has region_info.bounding_box
+    return data.faces.map((region) => {
       return {
-        leftCol: box.left_col * width,
-        topRow: box.top_row * height,
-        width: (box.right_col - box.left_col) * width,
-        height: (box.bottom_row - box.top_row) * height,
+        leftCol: region.left_col * width,
+        topRow: region.top_row * height,
+        rightCol: width - region.right_col * width,
+        bottomRow: height - region.bottom_row * height,
+        width: (region.right_col - region.left_col) * width,
+        height: (region.bottom_row - region.top_row) * height,
       };
     });
   };
@@ -69,66 +50,66 @@ class App extends Component {
   onInputChange = (event) => this.setState({ input: event.target.value });
 
   onImageLoad = () => {
-  if (this.lastClarifaiData && this.lastClarifaiData.outputs[0].data.regions) {
-    const boxes = this.calculateFaceLocations(this.lastClarifaiData);
-    this.displayFaceBoxes(boxes);
-  } else {
-    this.displayFaceBoxes([]); // no faces → clear boxes
-  }
-};
+    if (this.lastClarifaiData && this.lastClarifaiData.outputs[0].data.regions) {
+      const boxes = this.calculateFaceLocations(this.lastClarifaiData);
+      this.displayFaceBoxes(boxes);
+    } else {
+      this.displayFaceBoxes([]); // no faces → clear boxes
+    }
+  };
 
   onButtonSubmit = () => {
-  // Only proceed if the input URL is different from the currently displayed image
-  if (this.state.input === this.state.imageURL) return;
+    // Only proceed if the input URL is different from the currently displayed image
+    if (this.state.input === this.state.imageURL) return;
 
-  // Reset image and boxes
-  this.setState({ imageURL: this.state.input, boxes: [] });
-  this.lastClarifaiData = null;
+    // Reset image and boxes
+    this.setState({ imageURL: this.state.input, boxes: [] });
+    this.lastClarifaiData = null;
 
-  const MODEL_ID = "face-detection";
-  const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
+    fetch(
+      `https://smart-brain-api-spvs.onrender.com/imageurl`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    }).then((response) => response.json())
+      .then((result) => {
+        console.log("Clarifai result:", result);
+        this.lastClarifaiData = result;
 
-  fetch(
-    `/api/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`,
-    returnClarifaiRequestOptions(this.state.input)
-  )
-    .then((response) => response.json())
-    .then((result) => {
-      console.log("Clarifai result:", result);
-      this.lastClarifaiData = result;
+        // If faces detected, calculate boxes
+        if (result.faces) {
+          const boxes = this.calculateFaceLocations(result);
+          this.displayFaceBoxes(boxes);
 
-      // If faces detected, calculate boxes
-      if (result.outputs[0].data.regions) {
-        const boxes = this.calculateFaceLocations(result);
-        this.displayFaceBoxes(boxes);
-
-        const faceCount = boxes.length;
-
-        // update entries by number of faces
-        fetch(`https://smart-brain-api-spvs.onrender.com/image`, {
-          method: "put",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            id: this.state.user.id,
-            faces: faceCount,
-          }),
-        })
-          .then((res) => res.json())
-          .then((entries) =>
-            this.setState(Object.assign(this.state.user, { entries }))
-          )
-          .catch(console.log);
-      } else {
-        // No faces detected → clear boxes
+          const faceCount = boxes.length;
+          console.log('faceCount', faceCount)
+          // update entries by number of faces
+          fetch(`https://smart-brain-api-spvs.onrender.com/image`, {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+              faces: faceCount,
+            }),
+          })
+            .then((res) => res.json())
+            .then((entries) =>
+              this.setState(Object.assign(this.state.user, { entries }))
+            )
+            .catch(console.log);
+        } else {
+          // No faces detected → clear boxes
+          this.displayFaceBoxes([]);
+        }
+      })
+      .catch((err) => {
+        console.log("error", err);
+        // In case of error, clear boxes
         this.displayFaceBoxes([]);
-      }
-    })
-    .catch((err) => {
-      console.log("error", err);
-      // In case of error, clear boxes
-      this.displayFaceBoxes([]);
-    });
-};
+      });
+  };
 
   onRouteChange = (route) => {
     if (route === "signout") this.setState(initialState);
